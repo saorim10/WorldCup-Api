1cbac2e6ce91e888fcc09b8a33ff91fbcc6e5e2e
Saorim <meom@gft.com>
2026-03-18 15:41:17 -0300

    Criação do projeto

---
+++ src/test/java/com/saorim/WorldCup_Api/WorldCupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saorim.WorldCup_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldCupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
